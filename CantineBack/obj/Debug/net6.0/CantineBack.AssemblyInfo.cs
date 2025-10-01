6599ef5ad1bc54012969f00b4c2d3f646fa47958
OmarDIOP0 <odiop6170@gmail.com>
2025-10-01 08:42:39 +0000

    refonte de l'affichage de l_article

--- CantineBack/obj/Debug/net6.0/CantineBack.AssemblyInfo.cs
+++ CantineBack/obj/Debug/net6.0/CantineBack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantineBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f1218ed4c41583bad52d61d6bc701434f34402")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b403606941a37065dd9ee659735ba8e5c378eac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantineBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantineBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
