fcc4f7553375b03c07c5ba72425247ac31e298c0
OmarDIOP0 <odiop6170@gmail.com>
2025-10-06 21:37:11 +0000

    entreprise

--- CantineFront/obj/Debug/net6.0/CantineFront.AssemblyInfo.cs
+++ CantineFront/obj/Debug/net6.0/CantineFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantineFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a1b126708618f15c75dcefac510c15f363b0ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+307cebc9c9a0bfc0ad6523f45a880e7e89039f36")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantineFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantineFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
