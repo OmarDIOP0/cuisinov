c79078fa3fdf326ee07a13d09369159bf1ef25fb
OmarDIOP0 <odiop6170@gmail.com>
2025-10-26 14:19:54 +0000

    style commande

--- CantineFront/obj/Debug/net6.0/CantineFront.AssemblyInfo.cs
+++ CantineFront/obj/Debug/net6.0/CantineFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantineFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05c4e70449398c65307a822729f3b058dbb3ba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ba53d922552da758830fd7d9a845fc0acf3c0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantineFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantineFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
