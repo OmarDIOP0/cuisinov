7cd5e3e6712f34994dba29b8b1065f5ab682b049
OmarDIOP0 <odiop6170@gmail.com>
2025-11-03 20:54:30 +0000

    change and adding padding on body

--- CantineFront/obj/Debug/net6.0/CantineFront.AssemblyInfo.cs
+++ CantineFront/obj/Debug/net6.0/CantineFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantineFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88757d94da9696e7f0594744651edf013cc81cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937bd57a03780f0e9ad12659f6a68ede40f72a8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantineFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantineFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
