937bd57a03780f0e9ad12659f6a68ede40f72a8a
OmarDIOP0 <odiop6170@gmail.com>
2025-11-03 20:20:05 +0000

    styling the card order

--- DPWorldEncryption.Core/obj/Debug/netstandard2.0/DPWorldEncryption.Core.AssemblyInfo.cs
+++ DPWorldEncryption.Core/obj/Debug/netstandard2.0/DPWorldEncryption.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DPWorldEncryption.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0e5c90bd090baf900a6078e35a60d7813ad41d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88757d94da9696e7f0594744651edf013cc81cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DPWorldEncryption.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DPWorldEncryption.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
