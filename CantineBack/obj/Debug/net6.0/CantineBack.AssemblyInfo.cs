d4754bfb1e33cc7f1e7ad1b8b0debf8c12410119
OmarDIOP0 <odiop6170@gmail.com>
2025-11-04 16:05:52 +0000

    adding bs target on modal to change advice

--- CantineBack/obj/Debug/net6.0/CantineBack.AssemblyInfo.cs
+++ CantineBack/obj/Debug/net6.0/CantineBack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantineBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e795716bf24aedb41bf17428b193e9f6b98c7864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b7462b267805b5e8d2831f62d2c951da1c28fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantineBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantineBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
