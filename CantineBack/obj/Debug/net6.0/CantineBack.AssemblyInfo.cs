fe508527ca695c66a9b5bdd1cd0a0896ff91f7dd
OmarDIOP0 <odiop6170@gmail.com>
2025-11-08 08:04:51 +0000

    ajout du modal pour afficher le email

--- CantineBack/obj/Debug/net6.0/CantineBack.AssemblyInfo.cs
+++ CantineBack/obj/Debug/net6.0/CantineBack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantineBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072bfd844b897229a3ebe64388a51c3dd7bb39ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f687dadcef1753e6fed8ad2741c7cf652087a01c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantineBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantineBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
