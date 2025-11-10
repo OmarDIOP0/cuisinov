31df68d22b83b5ad89cd35a61e690d9965d7f624
OmarDIOP0 <odiop6170@gmail.com>
2025-11-10 23:49:03 +0000

    add new table for forget password link and generate token

---
+++ CantineBack.Models/PasswordResetToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CantineBack.Models
+{
+    public class PasswordResetToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = null!;
+        public DateTime ExpireAt { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; } = null!;
+    }
+}
--- CantineBack.Models/obj/Debug/net6.0/CantineBack.Models.AssemblyInfo.cs
+++ CantineBack.Models/obj/Debug/net6.0/CantineBack.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantineBack.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a2e56e8ff3e33ac7f8326c62b28e6be1eeffd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f1b77e2cac813419c2a66dadec6ee0795185411")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantineBack.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantineBack.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
