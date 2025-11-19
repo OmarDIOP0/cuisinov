385938e57451bbd9c2948602c1fe40ae850cec51
OmarDIOP0 <odiop6170@gmail.com>
2025-11-19 22:17:09 +0000

    ajouter des champs dans la modification

---
+++ CantineBack/Migrations/20251119215521_AddStartEndTimeToCategorie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CantineBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStartEndTimeToCategorie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EndTime",
+                table: "Categories",
+                type: "time",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "StartTime",
+                table: "Categories",
+                type: "time",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Categories");
+        }
+    }
+}
