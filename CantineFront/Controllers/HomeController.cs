94dc265c916dc6ebf075211de7462b716fda12ec
OmarDIOP0 <odiop6170@gmail.com>
2025-11-20 22:58:14 +0000

    gerer le cas du clic du bouton commander

--- CantineFront/Controllers/HomeController.cs
+++ CantineFront/Controllers/HomeController.cs
@@ -44,7 +44,7 @@ namespace CantineFront.Controllers
                     }
 
                 }
-                return View("Menu");
+                return View();
             }
 
             return RedirectToAction("Onboarding");
