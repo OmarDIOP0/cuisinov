9e580ae8d7f21429ea17d355502a1e64df375d88
OmarDIOP0 <odiop6170@gmail.com>
2025-11-21 18:19:20 +0000

    integration de l'entreprise dans le filtre des commandes

--- DPWorldEncryption.Core/obj/Debug/netstandard2.0/DPWorldEncryption.Core.AssemblyInfo.cs
+++ DPWorldEncryption.Core/obj/Debug/netstandard2.0/DPWorldEncryption.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DPWorldEncryption.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a542d52e5e82ccf38a14cd4529256f124ce2bc9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f619da976f633c4b9c9aa1cc5a12b10800adcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DPWorldEncryption.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DPWorldEncryption.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
